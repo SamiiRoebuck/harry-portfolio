bc6aa6a8db02166861e2837281433f4671022a0b
Samantha Roebuck <dlssr@leeds.ac.uk>
2019-05-14 15:59:24 +0100

    updated nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,48 +1,69 @@
-import React from 'react';
-import logo from '../images/logo.png'
+import React from "react"
+import logo from "../images/logo.png"
 import { Link } from "gatsby"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
 
 class NavBar extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {isToggleOn: true};
+    super(props)
+    this.state = { isToggleOn: true }
 
     // This binding is necessary to make `this` work in the callback
-    this.handleClick = this.handleClick.bind(this);
+    this.handleClickBurger = this.handleClickBurger.bind(this)
   }
 
-  handleClick() {
+  handleClickBurger() {
     this.setState(state => ({
-      isToggleOn: !state.isToggleOn
-    }));
+      isToggleOn: !state.isToggleOn,
+    }))
   }
+
 
   render() {
     return (
-
       <nav className="navbar">
-        <span className="navbar-toggle" id="js-navbar-toggle" onClick={this.handleClick}>
-        <FontAwesomeIcon icon={faBars} />
+      <div className="hoverLines"></div>
+        <span
+          className="navbar-toggle"
+          id="js-navbar-toggle"
+          onClick={this.handleClickBurger}
+        >
+          <FontAwesomeIcon icon={faBars} />
         </span>
-        <img src={logo} height="40"></img>
-        <ul className={`main-nav ${this.state.isToggleOn ? 'not-active' : 'active'}`}  id="js-menu">
+        <img src={logo} height="70" />
+        <ul
+          className={`main-nav ${
+            this.state.isToggleOn ? "not-active" : "active"
+          }`}
+          id="js-menu"
+
+          onClick={this.handleClickNav}
+        >
           <li>
-            <Link to="/" className="nav-links">Home</Link>
+            <Link to="/" className="nav-links" activeClassName="active-link">
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/" className="nav-links">Home</Link>
+            <Link to="/about" className="nav-links" activeClassName="active-link">
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/" className="nav-links">Home</Link>
+            <Link to="/" className="nav-links">
+              Portfolio
+            </Link>
           </li>
           <li>
-            <Link to="/" className="nav-links">Home</Link>
+            <Link to="/" className="nav-links">
+              Contact
+            </Link>
           </li>
           <li>
-            <Link to="/" className="nav-links">Home</Link>
+            <Link to="/" className="nav-links">
+              Download Portfolio
+            </Link>
           </li>
         </ul>
       </nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,8 @@ export default () => (
       <NavBar/>
     </div>
 
-    <NavMenu className="content" />
+    <main>
+      <NavMenu/>
+    </main>
   </div>
 )
