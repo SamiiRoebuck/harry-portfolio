2de296198707f6b9b26e09f1cc7405fab3f05b4e
Sam Roebuck <samiiroebuck.96@gmail.com>
2020-01-29 16:21:43 +0000

    fixing things I broke

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faInstagram, faLinkedin, faTwitter, faFacebookSquare } from "@fortawesome/free-brands-svg-icons"
 
+// import styles from '../styles/Footer.module.scss'
 
 function Footer() {
   return (
